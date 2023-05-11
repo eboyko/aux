e36dc648a2bd68993e2e5ff48266262cd7ee91ee
Evgeny Boyko <eboyko@eboyko.ru>
2023-05-11 17:50:38 +0300

    Add namespaced registered class spec

---
+++ spec/integration/pluggable/namespaced/registered_class_spec.rb
@@ -0,0 +1,9 @@
+require 'pluggable_helper'
+
+RSpec.describe(Namespaced::RegisteredClass) do
+  include_context 'with pluggable registry'
+
+  it 'presents in the registry' do
+    expect(registry.resolve('namespaced.registered_class')).to be(described_class)
+  end
+end
