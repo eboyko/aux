7898ab9e8bceeaca06d4834240fe65a83f3b240c
Evgeny Boyko <eboyko@eboyko.ru>
2023-05-11 18:57:37 +0300

    Simplify the check

--- spec/integration/pluggable/registered_class_spec.rb
+++ spec/integration/pluggable/registered_class_spec.rb
@@ -4,6 +4,6 @@ RSpec.describe(RegisteredClass) do
   include_context 'with pluggable registry'
 
   it 'presents in the registry' do
-    expect(registry.resolve(:registered_class)).to be(RegisteredClass)
+    expect(registry.resolve(:registered_class)).to be(described_class)
   end
 end
