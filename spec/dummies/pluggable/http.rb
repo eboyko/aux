a0960e0946e5ccfbebca2ec3acf352851c54c3b3
Evgeny Boyko <eboyko@eboyko.ru>
2023-05-18 19:11:29 +0300

    Restore ActiveSupport inflections and new HTTP module with HTMLParser class

---
+++ spec/dummies/pluggable/http.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+# Defines a non-standard named module
+module HTTP
+end
---
+++ spec/dummies/pluggable/http/html_parser.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module HTTP
+  class HTMLParser
+    include Aux::Pluggable
+
+    register
+  end
+end
