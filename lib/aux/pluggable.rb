07c688b7726cfd54943010f16243fdcff83136c9
Evgeny Boyko <eboyko@eboyko.ru>
2024-03-14 16:51:06 +0300

    Update pluggable module

--- lib/aux/pluggable.rb
+++ lib/aux/pluggable.rb
@@ -10,6 +10,8 @@ module Aux
   # Describes interface that makes any class able to register itself as well as resolve dependencies
   # rubocop:disable Style/ClassVars
   module Pluggable
+    @@registry = Aux::Registry.new
+
     # Extends the including class with ClassMethods and initializes a new Connector instance
     # @param base [Class] the class that includes this module
     def self.included(base)
@@ -20,12 +22,19 @@ module Aux
       end
     end
 
-    # @param registry [Dry::Container]
+    # @yield configure the module
+    # @yieldparam pluggable [Module<Aux::Pluggable>]
+    # @yieldparam registry [Aux::Registry]
+    def self.configure
+      yield(self, @@registry) if block_given?
+    end
+
+    # @param registry [Aux::Registry]
     def self.registry=(registry)
       @@registry = registry
     end
 
-    # @return [Dry::Container]
+    # @return [Aux::Registry]
     def self.registry
       @@registry
     end
