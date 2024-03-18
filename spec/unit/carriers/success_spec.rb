1f1c0777ce2ff14e964fcc0c9c4356244b8f4664
Evgeny Boyko <eboyko@eboyko.ru>
2024-03-18 19:13:18 +0300

    Add describe wrapper to units

--- spec/unit/carriers/success_spec.rb
+++ spec/unit/carriers/success_spec.rb
@@ -2,30 +2,32 @@ require 'spec_helper'
 require 'aux/carriers/success'
 
 RSpec.describe(Aux::Carriers::Success) do
-  subject(:success) { described_class.new(:created) }
+  describe '.new' do
+    subject(:success) { described_class.new(:created) }
 
-  it 'responds to predicate methods' do
-    expect(success).to be_succeeded
-    expect(success).to be_successful
-    expect(success).to be_success
-  end
+    it 'responds to predicate methods' do
+      expect(success).to be_succeeded
+      expect(success).to be_successful
+      expect(success).to be_success
+    end
 
-  it 'responds to negated predicate methods' do
-    expect(success).not_to be_failed
-    expect(success).not_to be_failure
-  end
+    it 'responds to negated predicate methods' do
+      expect(success).not_to be_failed
+      expect(success).not_to be_failure
+    end
 
-  it 'has code and empty payload' do
-    expect(success).to have_attributes(code: :created, payload: nil)
-  end
+    it 'has code and empty payload' do
+      expect(success).to have_attributes(code: :created, payload: nil)
+    end
 
-  context 'when both code and payload given' do
-    subject(:success) { described_class.new(:created, payload) }
+    context 'when both code and payload given' do
+      subject(:success) { described_class.new(:created, payload) }
 
-    let(:payload) { { id: 1 } }
+      let(:payload) { { id: 1 } }
 
-    it 'has code and payload' do
-      expect(success).to have_attributes(code: :created, payload: payload)
+      it 'has code and payload' do
+        expect(success).to have_attributes(code: :created, payload: payload)
+      end
     end
   end
 end
