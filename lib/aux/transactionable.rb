8d45aa4dd39bfa946acc5985b4e0f9fe2bc4bd88
Evgeny Boyko <eboyko@eboyko.ru>
2026-02-07 16:14:53 +0300

    Refactor Transactionable module into Aux namespace

--- lib/aux/transactionable.rb
+++ lib/aux/transactionable.rb
@@ -3,10 +3,12 @@
 # Provides a set of methods for safe execution within database transactions,
 # including support for nested transactions and advisory locking
 #
-module Transactionable
-  # @param options [Hash]
-  # @yield
-  def transaction(**options)
-    ActiveRecord::Base.transaction(**options) { yield }
+module Aux
+  module Transactionable
+    # @param options [Hash]
+    # @yield
+    def transaction(**options)
+      ActiveRecord::Base.transaction(**options) { yield }
+    end
   end
 end
