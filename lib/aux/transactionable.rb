864dd76613ad9513883242e03251866ea69e5101
Evgeny Boyko <eboyko@eboyko.ru>
2026-02-07 16:04:38 +0300

    Introduce very first transactionable module

---
+++ lib/aux/transactionable.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+# Provides a set of methods for safe execution within database transactions,
+# including support for nested transactions and advisory locking
+#
+module Transactionable
+  # @param options [Hash]
+  # @yield
+  def transaction(**options)
+    ActiveRecord::Base.transaction(**options) { yield }
+  end
+end
